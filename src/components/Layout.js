196196e051107090cacdf3308d8948b88d9343d4
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2023-04-19 22:54:26 +0500

    Added

---
+++ src/components/Layout.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+
+import Header from './sections/Header';
+import Footer from './sections/Footer';
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Next Tailwind Theme</title>
+        <meta
+          name="description"
+          content="Create dark mode in next and tailwind"
+        />
+      </Head>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="grow">{children}</main>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default Layout;
