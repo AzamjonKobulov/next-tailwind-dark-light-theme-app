3cf4e023b08c2bce4f1da98ada457ff124a6ae32
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2023-04-19 22:59:47 +0500

    Some changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider attribute="class" defaultTheme="light">
       <Layout>
         <Component {...pageProps} />
       </Layout>
